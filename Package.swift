72f3d2c2f62bb9ba94ad7eacfdcf5bf411a8ffd6
xander <xr>
2024-02-20 23:16:12 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "HandVector",
+    platforms: [.visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
