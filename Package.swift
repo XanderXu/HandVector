75e27371cbf1b5049e2edab21300bb8cccb58ee4
xuhf <517807635@qq.com>
2024-02-21 14:45:09 +0800

    update

--- Package.swift
+++ Package.swift
@@ -5,18 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "HandVector",
-    platforms: [.visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HandVector",
-            targets: ["HandVector"]
-        ),
+            targets: ["HandVector"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "HandVector"),
+        .target(
+            name: "HandVector"),
         .testTarget(
             name: "HandVectorTests",
             dependencies: ["HandVector"]),
